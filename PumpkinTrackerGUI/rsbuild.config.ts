6f47e33bc9a4386504f0f18d2db8c74ea5fdc81c
teknostom <evaldsson.tom@gmail.com>
2025-09-14 22:24:41 +0200

    fix imports

--- PumpkinTrackerGUI/rsbuild.config.ts
+++ PumpkinTrackerGUI/rsbuild.config.ts
@@ -4,6 +4,7 @@ import { pluginReact } from "@rsbuild/plugin-react";
 export default defineConfig({
 	plugins: [pluginReact()],
 	output: {
+		assetPrefix: "./",
 		cssModules: {
 			auto: true,
 			localIdentName: "[name]__[local]___[hash:base64:5]",
